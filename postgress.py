3e45bf11533f340036d01482c86755e7c9e1b2d1
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2024-03-24 22:57:19 +0200

    fix

--- postgress.py
+++ postgress.py
@@ -24,9 +24,16 @@ CREATE TABLE tasks (
   id SERIAL PRIMARY KEY,
   title VARCHAR(100),
   description TEXT,
-  FOREIGN KEY (status_id) REFERENCES status (id) ON DELETE SET NULL ON UPDATE CASCADE,
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL ON UPDATE CASCADE
+  status_id INT REFERENCES status (id) ON DELETE SET NULL ON UPDATE CASCADE,
+  user_id INT REFERENCES users (id) ON DELETE SET NULL ON UPDATE CASCADE
 );
+"""
+
+join_info_query = """
+SELECT tasks.id, tasks.title, tasks.description, status.name AS status_name, users.fullname AS user_name
+FROM tasks
+LEFT JOIN status ON tasks.status_id = status.id
+LEFT JOIN users ON tasks.user_id = users.id;
 """
 
 select_info_query = """
@@ -45,20 +52,24 @@ cursor.execute(create_table_query)
 connection.commit()
 cursor.execute(create_table_query1)
 connection.commit()
+
+# fill statuses
+status_data = ['new', 'in progress', 'completed']
+for status in status_data:
+    cursor.execute("INSERT INTO status (name) VALUES (%s)", (status,))
+connection.commit()
+# end fill statuses
+
 cursor.execute(create_table_query2)
 connection.commit()
 
-# fill
+# fill rest
 # 1
 for _ in range(10):
     fullname = fake.name()
     email = fake.email()
     cursor.execute("INSERT INTO users (fullname, email) VALUES (%s, %s)", (fullname, email))
-
-# 2
-status_data = ['new', 'in progress', 'completed']
-for status in status_data:
-    cursor.execute("INSERT INTO status (name) VALUES (%s)", (status,))
+    connection.commit()
 
 # 3
 task1 = ('do this', 'do this first', 1, 2)
@@ -68,14 +79,17 @@ cursor.execute(
     "VALUES (%s, %s, %s, %s)",
     (task1[0], task1[1], task1[2], task1[3])
 )
+connection.commit()
+
 cursor.execute(
     "INSERT INTO tasks (title, description, status_id, user_id) "
     "VALUES (%s, %s, %s, %s)",
     (task2[0], task2[1], task2[2], task2[3])
-)   
+)  
+connection.commit() 
 
-# select
-cursor.execute(select_info_query)
+# join
+cursor.execute(join_info_query)
 result = cursor.fetchall()
 print(result)
 
